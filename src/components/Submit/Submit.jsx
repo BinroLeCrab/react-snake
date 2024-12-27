eec9ec730d265879eb32be4268c0016cc104de7c
Robin Vigier / B!nro <rob.vigier@gmail.com>
2024-12-27 15:30:26 +0100

    🐛✨ score in local storage

--- src/components/Submit/Submit.jsx
+++ src/components/Submit/Submit.jsx
@@ -12,7 +12,7 @@ const Submit = ({ score, death }) => {
 
         let results = localStorage.getItem("results");
         results = JSON.parse(results);
-        
+
         const newResults = {
             name: name,
             score: score,
@@ -21,8 +21,18 @@ const Submit = ({ score, death }) => {
 
         if (results === null) {
             results = [];
-            results.push(newResults);
-        } 
+        } else {
+            let alreadyExist = results.find(
+                    (score) => score.name === newResults.name
+                );
+            if (alreadyExist) {
+                results = results.filter(
+                    (score) => score.name !== newResults.name
+                );
+            }
+        }
+
+        results.push(newResults);
 
         localStorage.setItem("results", JSON.stringify(results));
     };
@@ -40,3 +50,17 @@ const Submit = ({ score, death }) => {
 }
 
 export default Submit;
+
+// const item = getter.find(
+//     (_item) => _item.x === head[0] && _item.y === head[1]
+// );
+
+// if (item) {
+
+//     const newItemArray = getter.filter(_item => _item !== item);
+//     setter(newItemArray);
+
+//     return true;
+// } else {
+//     return false;
+// }
