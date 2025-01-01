c99b112cf5814c45b361104da1503a6ad5e06bf7
Robin Vigier / B!nro <rob.vigier@gmail.com>
2025-01-01 19:50:44 +0100

    💄 Pause screen

--- src/components/PauseScreen/PauseScreen.jsx
+++ src/components/PauseScreen/PauseScreen.jsx
@@ -5,16 +5,21 @@ const PauseScreen = ({ quitPause, setPlay, restart, chill, gamePaused, canvasRef
     return (
         <>
             <div className={s.pauseScreen}>
-                <h1>Jeu en pause</h1>
-                <button onClick={quitPause}>Reprendre</button>
-                <button onClick={() => setPlay(false)}>Quitter</button>
-                {chill && (
-                    <>
-                        <button onClick={restart}>Recommencer</button>
-                        <CanvasImage gamePaused={gamePaused} canvasRef={canvasRef} />
-                    </>
-                )}
-                <p>Ou presser la touche espace pour reprendre.</p>
+                <h1 className={s["pauseScreen__title"]}>Jeu en pause</h1>
+                <div className={s["pauseScreen__content"]}>
+                    {chill && (
+                        <div className={s["pauseScreen__column"]}>
+                            <CanvasImage gamePaused={gamePaused} canvasRef={canvasRef} />
+                        </div>
+                    )}
+                    <div className={`${s["pauseScreen__column"]} ${s["pauseScreen__column--btn"] }`}>
+                        <button onClick={quitPause}>Reprendre</button>
+                        {chill && <button onClick={restart}>Recommencer</button>}
+                        <button onClick={() => setPlay(false)}>Quitter</button>
+                        <p>Ou pressez la touche espace pour reprendre.</p>
+                    </div>
+
+                </div>
             </div>
             <div className={s.crt}></div>
         </>
